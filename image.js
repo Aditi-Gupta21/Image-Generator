06392bf0828df1a90547e253ab74e832dc67124f
Aditi <aditigupta.21apr@gmail.com>
2023-08-02 23:57:03 +0530

    Added javascript

--- image.js
+++ image.js
@@ -1,11 +1,41 @@
+const key="g1TuyAB-wMw0ccgT_sMQuFe4l9Gchi10K9u_WqR7Dlc";
 let input = document.getElementById("input-search");
 let search = document.getElementById("Search");
-let more = document.getElementById("More-img")
+let searchResultEl = document.getElementById("results");
+let more = document.getElementById("More-img");
+let page =1;
+// if(page===1){
+//     searchResultEl.innerHTML = "";
+// }
 search.addEventListener('click',(event)=>{
     event.preventDefault();
     let inputEl=input.value;
-    console.log(inputEl)
+    page =1;
+    searchImg(inputEl);
 })
-more.addEventListener('click',()=>{
-    console.log("more");
-})
+async function searchImg(input){
+    console.log(input);
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${input}&client_id=${key}`;
+    const response = await fetch(url);
+    if(!response.ok){
+        throw new Error("Network error")
+    }
+    const data = await response.json();
+    const results = data.results;
+    results.map((result)=>{
+        const imgWrapper = document.createElement("div");
+        imgWrapper.classList.add("res");
+        const img = document.createElement("img");
+        img.src = result.urls.small;
+
+        imgWrapper.appendChild(img);
+        searchResultEl.appendChild(imgWrapper);
+    })
+    console.log(results);
+}
+
+
+
+// more.addEventListener('click',()=>{
+//     console.log("more");
+// })
